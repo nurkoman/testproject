301c200e857c0840d74d02f445160f076773f396
Mirosław Kopeć <miroslaw.kopec@lppsa.com>
2020-05-22 15:05:34 +0200

    add Calculator class with test

---
+++ src/test/java/pl/emkey/calkulator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package pl.emkey.calkulator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+
+}
