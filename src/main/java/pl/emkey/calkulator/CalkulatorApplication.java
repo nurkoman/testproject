58f9364a8abec0c33a9c9435b6a573457f570e13
Mirosław Kopeć <miroslaw.kopec@lppsa.com>
2020-05-22 14:45:17 +0200

    initial commit

---
+++ src/main/java/pl/emkey/calkulator/CalkulatorApplication.java
@@ -0,0 +1,13 @@
+package pl.emkey.calkulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalkulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalkulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/emkey/calkulator/CalkulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.emkey.calkulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalkulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
